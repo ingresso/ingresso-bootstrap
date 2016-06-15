318fbfc620fa0e8e17a50b6aef824d3a3825d06a
Arthur Franco <arthurpf4@gmail.com>
2016-06-15 19:37:16 -0300

    criando plugin slide-page

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -2064,13 +2064,7 @@ var Modal = (function ($) {
     }, {
       key: '_checkScrollbar',
       value: function _checkScrollbar() {
-        var fullWindowWidth = window.innerWidth;
-        if (!fullWindowWidth) {
-          // workaround for missing window.innerWidth in IE8
-          var documentElementRect = document.documentElement.getBoundingClientRect();
-          fullWindowWidth = documentElementRect.right - Math.abs(documentElementRect.left);
-        }
-        this._isBodyOverflowing = document.body.clientWidth < fullWindowWidth;
+        this._isBodyOverflowing = document.body.clientWidth < window.innerWidth;
         this._scrollbarWidth = this._getScrollbarWidth();
       }
     }, {
@@ -3562,13 +3556,5 @@ var Popover = (function ($) {
 
   return Popover;
 })(jQuery);
-
-$("body").on("input propertychange", ".fl-form-group", function (e) {
-  $(this).toggleClass("fl-form-group-with-value", !!$(e.target).val());
-}).on("focus", ".fl-form-group", function () {
-  $(this).addClass("fl-form-group-with-focus");
-}).on("blur", ".fl-form-group", function () {
-  $(this).removeClass("fl-form-group-with-focus");
-});
 
 }(jQuery);
